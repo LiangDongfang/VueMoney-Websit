73714b8a4a3bc12e76fe192bbf7613af4a9c99de
liangdongfang <781098813@qq.com>
2020-06-16 19:50:52 +0800

    deploy

---
+++ precache-manifest.9e97cbbb0ad582a9cb87a50f8020de4f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51d941eaea8768191ab7",
+    "url": "/VueMoney-Website/css/app.cfada3fc.css"
+  },
+  {
+    "revision": "e2d94ef50de28e40e2ea38e157aa2e88",
+    "url": "/VueMoney-Website/index.html"
+  },
+  {
+    "revision": "51d941eaea8768191ab7",
+    "url": "/VueMoney-Website/js/app.345faf1a.js"
+  },
+  {
+    "revision": "79865b7265c65499ff02",
+    "url": "/VueMoney-Website/js/chunk-vendors.07018703.js"
+  },
+  {
+    "revision": "e37bc81a74b587acb39fab3753b1ea61",
+    "url": "/VueMoney-Website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/VueMoney-Website/robots.txt"
+  }
+]);
